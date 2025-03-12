2b33a512da629ab3fbc5bdee5fc2427c2f0a7704
Pratyoos Panta <pratyoos.panta@outlook.com>
2025-03-12 08:32:20 +0545

    Minor code changes

--- Lab 4 (Recursion)/towersOfHanoi.c
+++ Lab 4 (Recursion)/towersOfHanoi.c
@@ -2,16 +2,16 @@
 
 #include <stdio.h>
 
-void toh(int n, char src, char dst, char tmp)
+void toh(int n, char src, char tmp, char dst)
 {
     if (n == 1)
     {
         printf("Move disk 1 from %c to %c\n", src, dst);
         return;
     }
-    toh(n - 1, src, tmp, dst);
+    toh(n - 1, src, dst, tmp);
     printf("Move disk %d from %c to %c\n", n, src, dst);
-    toh(n - 1, tmp, dst, src);
+    toh(n - 1, tmp, src, dst);
 }
 
 int main()
@@ -19,6 +19,6 @@ int main()
     int n;
     printf("Enter the number of disks: ");
     scanf("%d", &n);
-    toh(n, 'A', 'C', 'B');
+    toh(n, 'A', 'B', 'C');
     return 0;
 }
