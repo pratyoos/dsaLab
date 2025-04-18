fc386c82444b98aff2f159539edaf320926360f0
Pratyoos Panta <pratyoos.panta@outlook.com>
2025-04-18 06:51:19 +0545

    Final Commit

--- Lab 6 (Sorting in C)/heapSort.c
+++ Lab 6 (Sorting in C)/heapSort.c
@@ -1,60 +1,60 @@
-// heap sort in c program
-// O(nlogn)
+// heap sort
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-void heapify(int A[], int n, int i) {
+void heapify(int arr[], int n, int i) {
     int largest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
-    if (left < n && A[left] > A[largest])
+
+    if (left < n && arr[left] > arr[largest]) {
         largest = left;
-    if (right < n && A[right] > A[largest])
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
         largest = right;
+    }
+
     if (largest != i) {
-        int temp = A[i];
-        A[i] = A[largest];
-        A[largest] = temp;
-        heapify(A, n, largest);
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+        heapify(arr, n, largest);
     }
 }
 
-void heapSort(int A[], int n) {
-    int i;
-    for (i = n / 2 - 1; i >= 0; i--)
-        heapify(A, n, i);
-    for (i = n - 1; i > 0; i--) {
-        int temp = A[0];
-        A[0] = A[i];
-        A[i] = temp;
-        heapify(A, i, 0);
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        heapify(arr, i, 0);
     }
 }
 
-void display(int A[], int n) {
-    int i;
-    for (i = 0; i < n; i++) {
-        printf("%d ", A[i]);
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
 int main() {
-    int A[100000], n, i;
-    clock_t start, end, diff;
-    printf("Enter n: ");
+    int n, i, arr[100];
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
-    for (i = 0; i < n; i++)
-        A[i] = rand();
-    display(A,n);
-    start = clock();
-    heapSort(A, n);
-    end = clock();
-    display(A,n);
-    diff = end - start;
-    printf("The time taken is %f seconds.\n", (double)diff / CLOCKS_PER_SEC);
+    // random number generator
+    for (i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+    display(arr, n);
+    heapSort(arr, n);
+    display(arr, n);
     return 0;
 }
 
